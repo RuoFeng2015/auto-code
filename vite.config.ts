e5ddf4be88488a79d271fcf61944d6774d22ab62
ruofeng <1652836476@qq.com>
2021-12-08 14:32:37 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   build: {
     // brotliSize: false, //关闭打包计算
     outDir: './docs',
