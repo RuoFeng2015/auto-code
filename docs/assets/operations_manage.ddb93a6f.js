d6a9f19a9713c4c20f99617876cc6ad95cfc2bf6
ruofeng <1652836476@qq.com>
2022-05-06 17:00:09 +0800

    aad

--- docs/assets/operations_manage.ddb93a6f.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.47bca9f6.js";import{T as t,o as r,c,V as a}from"./vendor.b23ac7e6.js";const n={},s={class:"activity_profile"};function _(i,m){const e=t("RouterView");return r(),c("div",s,[a(e)])}var l=o(n,[["render",_]]);export{l as default};
---
+++ docs/assets/statistic_analysis.168960e1.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index.44756176.js";import{o as t,c as a}from"./vendor.b23ac7e6.js";const c={},e={class:"statistic_analysis"};function i(o,r){return t(),a("div",e,"statistic_analysis")}var l=s(c,[["render",i]]);export{l as default};
